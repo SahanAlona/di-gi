e8af2e72e827e46d090337e5450e160f81d32098
SahanAlona <sagan.alona201408@gmail.com>
2021-10-21 12:08:20 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(function () {
+    $('.top-slider').slick({
+        dots: true,
+        infinite: false,
+        nextArrow:
+            '<button type="button" class="slick-arrow slick-next"><img src="./images/next.svg" alt="next arrow"></button>',
+        prevArrow:
+            '<button type="button" class="slick-arrow slick-prev"><img src="./images/prev.svg" alt="prev arrow"></button>',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {},
+            },
+
+            {
+                breakpoint: 640,
+                settings: {
+                    arrows: false,
+                },
+            },
+        ],
+    });
+
+    $('.testimonial-slider').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {},
+            },
+
+            {
+                breakpoint: 640,
+                settings: {
+                    arrows: false,
+                },
+            },
+        ],
+    });
+});
+
+// Masonry for Latest-news__items
+let elem = document.querySelector('.latest-news__items');
+let msnry = new Masonry(elem, {
+    // options
+    itemSelector: '.latest-news__item',
+    columnWidth: 270,
+    horizontalOrder: true,
+    fitWidth: true,
+    gutter: 30,
+});
